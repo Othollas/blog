c444de664e2d76caacfcfb651f5f4cb5717e85eb
o_thollas <o_thollas@hotmail.com>
2025-08-10 16:54:17 +0200

    Ajout de la fonction de logout

--- logout.php
+++ logout.php
@@ -1,1 +1,9 @@
 <!-- script qui permet de se deconnecter, grace à un session destroy -->
+
+<?php 
+    function logout(){
+        unset($_SESSION['auth_token']);
+        unset($_SESSION["user_id"]);
+        session_destroy();
+    };
+?>
