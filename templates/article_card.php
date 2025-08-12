45edc2758a1ea3f44bb5dab0ecb8524f6f05ac45
o_thollas <o_thollas@hotmail.com>
2025-08-12 17:38:09 +0200

    Modification des class bootstrap pour amélioration visuelle

--- templates/article_card.php
+++ templates/article_card.php
@@ -11,51 +11,39 @@ function card(mixed $array): mixed
     global $is_admin;
 
 
- 
-
-
     if ($is_admin) {
 
-   return "<div class='group-card'>
-    <article class='' style='margin:1rem; border:1px, solid, black; height:300px; width:250px; text-align:center'>
-        <a class='' href=article.php?id=" . $array["id"] . ">
-            <div class=''>
-                <img style='width:200px; height:200px; background-color:black' src='./uploads/images/" .  $array["img_url"] . "'  alt=''>
-            </div>
-            <div class=''>
-
-                <p style='text-align:center;width:200px; background-color:black'>" . $array['title'] . "</p>
-                <div class=''>
-                    <p>Par : " . $array["author_id"] . "</p>
+return "<div class='card h-100 shadow p-2 mb-2 rounded d-flex flex-column'>
+            <a class='text-decoration-none text-dark flex-grow-1' href='article.php?id=" . $array["id"] . "'>
+                <div class='card-body text-center d-flex flex-column h-100 justify-content-between'>
+                    <h5 class='card-title'>" . $array['title'] . "</h5>
+                    <div class='flex-grow-1 d-flex align-items-center justify-content-center py-2'>
+                        <img class='img-fluid mh-100' style='max-height: 150px; object-fit: contain' src='./uploads/images/" . $array["img_url"] . "' alt=''>
+                    </div>
+                    <p class='mb-0 mt-auto'>Par : " . $array["username"] . "</p>
                 </div>
-                     <nav><a href='./admins/edit.php?id=" . $array["id"] . "'>Modifier</a></nav>
-                    <nav><a href='./admins/delete.php?id=" . $array["id"] . "'>Effacer</a></nav>
+            </a>
+            <div class='card-footer bg-transparent border-0 py-2'>
+                <div class='d-flex justify-content-around'>
+                    <a class='btn btn-sm btn-outline-primary px-3' href='./admins/edit.php?id=" . $array["id"] . "'>Modifier</a>
+                    <a class='btn btn-sm btn-outline-danger px-3' href='./admins/delete.php?id=" . $array["id"] . "'>Effacer</a>
+                </div>
             </div>
-        </a>
-    </article>
-</div>";
-
+        </div>";
     } else {
-        return "<div class='group-card'>
-    <article class='' style='margin:1rem; border:1px, solid, black; height:300px; width:250px; text-align:center'>
-        <a class='' href=article.php?id=" . $array["id"] . ">
-            <div class=''>
-                <img style='width:200px; height:200px; background-color:black' src='./uploads/images/" .  $array["img_url"] . "'  alt=''>
-            </div>
-            <div class=''>
-
-                <p style='text-align:center;width:200px; background-color:black'>" . $array['title'] . "</p>
-                <div class=''>
-                    <p>Par : " . $array["author_id"] . "</p>
-                   
-                </div>
-                    
-            </div>
-        </a>
-    </article>
-</div>";
+        return "<div class='card h-100 shadow p-2 mb-2 rounded'>
+                    <a class='text-decoration-none text-dark h-100 d-block' href=article.php?id=" . $array["id"] . " >
+                        <div class='card-body text-center d-flex flex-column h-100 justify-content-between'>
+                            <h5 class='card-title'>" . $array['title'] . "</h5>
+                            <img class='card-img-top img-fluid'  src='./uploads/images/" . $array["img_url"] . "'  alt=''>
+                            <p class='mb-2'>Par : " . $array["username"] . "</p>
+                        </div>
+                    </a>
+                </div>";
     }
 }
+
+
 
 
 
