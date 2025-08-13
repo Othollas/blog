f3f43a6a2b8dae83fd6fc43b34e4b48347230680
o_thollas <o_thollas@hotmail.com>
2025-08-13 17:26:16 +0200

    Ajout de la verification du hash du password

--- login.php
+++ login.php
@@ -30,20 +30,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = strip_tags(trim($_POST['username']));
     $password = strip_tags(trim($_POST['password']));
     require_once './includes/db.php';
-    
+
     // recuperer le mail et le username
     $stmt = $pdo->prepare("SELECT id, email, password FROM USER where is_admin = true");
     $stmt->execute();
     $admin = $stmt->fetch(); // Si plusieurs admins mettre fetchAll()
 
-    if ($admin['email'] === $username && $admin['password'] === $password) {
-
-
-
+    
+    if ($admin['email'] === $username && password_verify($password, $admin['password'])) {
 
         if (authenticate($username, $password)) {
-             $_SESSION['auth_token'] = generateAuthToken($admin['id']);
-             $_SESSION['user_id'] = $admin['id'];
+            $_SESSION['auth_token'] = generateAuthToken($admin['id']);
+            $_SESSION['user_id'] = $admin['id'];
             header('Location: ./admins/dashboard.php');
         };
     }
@@ -57,27 +55,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 include_once './templates/header.php';
 ?>
 
+<div class="container text-center vh-100 border">
+    <h3 class="m-5">LOGIN</h3>
 
-<form action="./login.php" method="post">
+    <form action="./login.php" method="post" class="container">
 
-    <div class="form-group">
-        <label for="username">Entre ton pseudo</label>
-        <input type="text" name="username" id="username" autocomplete="false" required>
+        <div class="input-group mb-5 col-xl-3">
+            <span class="input-group-text p-4" id="inputGroup-sizing-default">Entre ton pseudo</span>
 
-    </div>
+            <input type="text" class="form-control" name="username" id="username" autocomplete="false" required aria-label="user input" aria-describedby="inputGroup-sizing-default">
 
-    <div class="form-group">
-        <label for="password">Rentre ton mot de passe</label><input type="password" name="password" id="password" autocomplete="false" required>
-    </div>
-    <?php
+        </div>
 
-    echo $error_empty ? '<p style="color:red">Vous devez entré un pseudo/password </p>' : null;
-    echo $error_form ?  '<p style="color:red"> Erreur email/password </p>' : null;
-    ?>
-    <div class="form-btn">
-        <button type="submit">Envoyer</button>
-    </div>
+        <div class="input-group mb-5 col-sm-3 ">
+            <span class="input-group-text p-4" id="inputGroup-sizing-default">Rentre ton mot de passe</span>
+            <input type="password" name="password" id="password" autocomplete="false" required class="form-control" aria-label="password input" aria-describedby="inputGroup-sizing-default">
+        </div>
 
-</form>
+
+        <?php
+
+        echo $error_empty ? '<p class="text-danger">Vous devez entré un pseudo/password </p>' : null;
+        echo $error_form ?  '<p class="text-danger"> Erreur email/password </p>' : null;
+        ?>
+
+        <button class="btn btn-primary w-100 p-4 mb-5" type="submit">Envoyer</button>
+
+
+    </form>
+</div>
 
 <?php include_once './templates/footer.php' ?>
