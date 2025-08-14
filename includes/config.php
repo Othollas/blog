2cf0c60e9d23d89c6b48998ac6b8edc901d57b64
o_thollas <o_thollas@hotmail.com>
2025-08-14 12:21:48 +0200

    test

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+  <?php 
+    $DB_HOST = 'localhost';
+    $DB_NAME = 'blog';
+    $DB_USER = 'root';
+    $DB_PASS = 'root';
+
+    $SECRET_KEY = 'JaimeB1enLesSaucissesMaisCPasTr0pCh0d';
+    $CHUT = "chut";
+  ?>
+  
